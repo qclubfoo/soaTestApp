9f8b1af58fe5e16bbe4a035fa00ce7da3afa0740
Dmitrii <dp20121989@mail.ru>
2020-11-16 15:56:29 +0300

    Initial Commit

---
+++ SoaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoaTest
+//
+//  Created by Дмитрий on 16.11.2020.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
